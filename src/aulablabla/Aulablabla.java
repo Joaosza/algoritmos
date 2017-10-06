119dd2489b42f050ac15fa425215b7979f079dde
Aluno <Aluno@172.32.1.14>
2017-10-05 22:42:39 -0300

    bla

---
+++ src/aulablabla/Aulablabla.java
@@ -0,0 +1,20 @@
+
+package aulablabla;
+
+
+public class Aulablabla {
+public static void quadrado(double n){
+double r = n * n;
+
+}
+   
+    public static void main(String[] args) {
+        double raiz;
+        
+        raiz = Math.sqrt(15);
+        System.out.println("a raiz e "+raiz);
+        
+        
+    }
+    
+}
